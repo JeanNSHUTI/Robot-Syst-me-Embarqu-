86763e003cd0a5c7e9247f76e7c7c0ffa6eb4d0e
JeanNSHUTI <16186@ecam.be>
2017-10-17 17:42:28 +0200

    Update ultrason.py
    Testing

--- ultrason.py
+++ ultrason.py
@@ -1,53 +1,37 @@
-#Libraries
-import RPi.GPIO as GPIO
-import time
- 
-#GPIO Mode (BOARD / BCM)
-GPIO.setmode(GPIO.BCM)
- 
-#set GPIO Pins
-GPIO_TRIGGER = 24
-GPIO_ECHO = 23
- 
-#set GPIO direction (IN / OUT)
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-GPIO.setup(GPIO_ECHO, GPIO.IN)
- 
-def distance():
-    # set Trigger to HIGH
-    GPIO.output(GPIO_TRIGGER, True)
- 
-    # set Trigger after 0.01ms to LOW
-    time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER, False)
- 
-    StartTime = time.time()
-    StopTime = time.time()
- 
-    # save StartTime
-    while GPIO.input(GPIO_ECHO) == 0:
-        StartTime = time.time()
- 
-    # save time of arrival
-    while GPIO.input(GPIO_ECHO) == 1:
-        StopTime = time.time()
- 
-    # time difference between start and arrival
-    TimeElapsed = StopTime - StartTime
-    # multiply with the sonic speed (34300 cm/s)
-    # and divide by 2, because there and back
-    distance = (TimeElapsed * 34300) / 2
- 
-    return distance
- 
-if __name__ == '__main__':
-    try:
-        while True:
-            dist = distance()
-            print ("Measured Distance = %.1f cm" % dist)
-            time.sleep(1)
- 
-        # Reset by pressing CTRL + C
-    except KeyboardInterrupt:
-        print("Measurement stopped by User")
-        GPIO.cleanup()
+import RPi.GPIO as GPIO                    #Import GPIO library
+import time                                #Import time library
+GPIO.setmode(GPIO.BCM)                     #Set GPIO pin numbering 
+
+TRIG = 23                                  #Associate pin 23 to TRIG
+ECHO = 24                                  #Associate pin 24 to ECHO
+
+print "Distance measurement in progress"
+
+GPIO.setup(TRIG,GPIO.OUT)                  #Set pin as GPIO out
+GPIO.setup(ECHO,GPIO.IN)                   #Set pin as GPIO in
+
+while True:
+
+  GPIO.output(TRIG, False)                 #Set TRIG as LOW
+  print "Waitng For Sensor To Settle"
+  time.sleep(2)                            #Delay of 2 seconds
+
+  GPIO.output(TRIG, True)                  #Set TRIG as HIGH
+  time.sleep(0.00001)                      #Delay of 0.00001 seconds
+  GPIO.output(TRIG, False)                 #Set TRIG as LOW
+
+  while GPIO.input(ECHO)==0:               #Check whether the ECHO is LOW
+    pulse_start = time.time()              #Saves the last known time of LOW pulse
+
+  while GPIO.input(ECHO)==1:               #Check whether the ECHO is HIGH
+    pulse_end = time.time()                #Saves the last known time of HIGH pulse 
+
+  pulse_duration = pulse_end - pulse_start #Get pulse duration to a variable
+
+  distance = pulse_duration * 17150        #Multiply pulse duration by 17150 to get distance
+  distance = round(distance, 2)            #Round to two decimal points
+
+  if distance > 2 and distance < 400:      #Check whether the distance is within range
+    print "Distance:",distance - 0.5,"cm"  #Print distance with 0.5 cm calibration
+  else:
+    print "Out Of Range"                   #display out of range
