9a6db4249b15ea9d7a43380c670464df3556a5dd
JeanNSHUTI <16186@ecam.be>
2017-10-18 11:48:32 +0200

    Update main3.py

--- main3.py
+++ main3.py
@@ -141,10 +141,10 @@ def stop():
 def ultrason() :
 	GPIO.output(TRIG, False)                 #Set TRIG as LOW
 	print "Waitng For Sensor To Settle"
-	time.sleep(0.1)                            #Delay of 2 seconds
+	sleep(0.001)                            #Delay of 2 seconds
   	GPIO.output(TRIG, True)                  #Set TRIG as HIGH
 
-	time.sleep(0.00001)                      #Delay of 0.00001 seconds
+	sleep(0.00001)                      #Delay of 0.00001 seconds
   	GPIO.output(TRIG, False)                 #Set TRIG as LOw
   	
 	while GPIO.input(ECHO)==0:               #Check whether the ECHO is LOW
